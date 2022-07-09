fc51c4446465df5a03127910afa6cebc0165e6fb
M.Kristóf <drzero123456@gmail.com>
2022-07-09 20:43:04 +0200

    js basics

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
 const form = document.querySelector("form");
-console.log(form);
+
+const email = document.getElementById("email");
+const country = document.getElementById("country");
+const zipcode = document.getElementById("zipcode");
+const password = document.getElementById("password");
+const password2 = document.getElementById("password2");
+
+email.addEventListener("input", (event) => {
+  const error = document.getElementById(event.target.id + "-error");
+  console.log(error);
+  if (email.validity.valid) {
+    error.className = "error";
+    error.textContent = "";
+  } else {
+    error.textContent = "Invalid email";
+  }
+});
